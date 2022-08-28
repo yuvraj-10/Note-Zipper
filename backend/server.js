cd66ac4e8b942c2b2aa70825b042149ba91e9d09
yuvraj-10 <yuvraj3901@gmail.com>
2022-08-29 03:04:49 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -22,19 +22,16 @@ app.use("/api/users", userRoutes);
 // --------------------------deployment------------------------------
 const __dirname = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config({ path: "../.env" });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
 }
 // --------------------------deployment------------------------------
 
@@ -51,11 +48,3 @@ app.listen(
       .bold
   )
 );
-
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
-module.exports=app;
-
