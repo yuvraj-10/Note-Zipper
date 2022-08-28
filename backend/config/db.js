08529add4eae94d002911135f71787baad5ce7d5
yuvraj-10 <yuvraj3901@gmail.com>
2022-08-29 03:45:17 +0530

    final

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ import colors from "colors";
 //require('dotenv').config({path: './config/config.env'})
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI="mongodb+srv://yuvraj10:yuvraj10@cluster0.pboi3eh.mongodb.net/?retryWrites=true&w=majority", {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
